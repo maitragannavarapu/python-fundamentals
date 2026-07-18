c0a3f84ecc45d0c5d42a1d185db94c0e738023fb
G . Sai Maitra <maitragannavarapu@gmail.com>
2026-07-18 14:38:11 +0530

    add positive negative zero checker

---
+++ Basics/positive_negative_zero.py
@@ -0,0 +1,14 @@
+"""
+Program: Positive/Negative/Zero
+Author: G. Sai Maitra
+Description: Checks whether a given number is positive, negative, or zero.
+"""
+
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("The number is positive.")
+elif n < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
