c01b0610fb3a9b6b2eaebf6c96a2b5285d978b0d
G . Sai Maitra <maitragannavarapu@gmail.com>
2026-07-18 14:28:45 +0530

    Add even or odd number checker

---
+++ Basics/even_odd.py
@@ -0,0 +1,12 @@
+"""
+program: even or odd number
+author: G.sai maitra
+description: checks whether a number is even or odd.
+"""
+n = int(input("Enter a number:"))
+if n % 2 == 0:
+    print("The given number is even number.")
+
+else:
+    print("The given number is odd number.")
+    
